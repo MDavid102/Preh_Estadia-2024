77811d2ebcd987fe705f6c7f8d0b6aed89c718e6
MDavid102 <130815811+MDavid102@users.noreply.github.com>
2024-01-05 11:41:46 -0600

    Add files via upload
    This carpet is to upload exercises and homework of Preh

---
+++ codes and exercises estadia preh/01_testprogram/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: Jan 3, 2024
+ *      Author: Angel Sanchez
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, World! \n");
+	printf("press 'ENTER' to exit this application");
+
+	getchar();
+
+	return 0;
+}
